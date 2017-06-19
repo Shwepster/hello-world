e8971534b8ddde912f546e3dcb7435dd96987538
Mex\MeeeX <Shwepster@gmail.com>
2017-06-19 18:53:26 +0300

    font size incresed

--- gitTest/Form1.Designer.cs
+++ gitTest/Form1.Designer.cs
@@ -81,6 +81,7 @@
             // 
             // label4
             // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.Location = new System.Drawing.Point(6, 99);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(213, 30);
